29526e8d3e43cc4c793c77d603477cf0f3494b54
Kunal Singh <kunal_singh@outlook.com>
2024-12-03 13:22:34 +0530

    updated value and added comment

--- rpg.py
+++ rpg.py
@@ -8,11 +8,11 @@ def game():
     while player_health > 0 and monster_health > 0:
         action=input("Choose 'attack' or 'heal'.\n")
         if action == "attack":
-            damage_dealt = random.randint(5, 10)
+            damage_dealt = random.randint(5, 8)
             monster_health -= damage_dealt
             print(f"You attacked and dealt {damage_dealt} damage.")
         elif action == "heal":
-            heal= random.randint(5, 10)
+            heal= random.randint(5, 12)
             player_health += heal
             print(f"You healed yourself for {heal} hp.")
         else:
@@ -21,9 +21,10 @@ def game():
     
 
         if monster_health > 0:
-            damage_dealt = random.randint(5, 10)
+            damage_dealt = random.randint(4, 11)
             player_health -= damage_dealt
             print(f"Monster attacked you and dealt {damage_dealt} damage")
+            #health indicator after each attack 
             print(f"your health {player_health} | monster health {monster_health}")
 
 
