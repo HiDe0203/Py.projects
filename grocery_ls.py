76199c7759b2ab6cf491ea491b333f5fe62ccfd0
Kunal Singh <kunal_singh@outlook.com>
2024-12-04 14:35:35 +0530

    display menu

---
+++ grocery_ls.py
@@ -0,0 +1,10 @@
+def display_menu():
+    print("\nGrocery List Manager")
+    print("1. View List")
+    print("2. Add item")
+    print("3. Remove item")
+    print("4. Sort list")
+    print("5. Search for an item")
+    print("6. Exit")
+
+display_menu()
