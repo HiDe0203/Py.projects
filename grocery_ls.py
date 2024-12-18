f72aa295d1716ab1a96acf3fb5b7832180bc2179
Kunal Singh <kunal_singh@outlook.com>
2024-12-18 15:38:36 +0530

    wip
    added c1,2,3

--- grocery_ls.py
+++ grocery_ls.py
@@ -12,3 +12,27 @@ def main():
 
     while True:
         display_menu()
+        choice = input("Enter your choice(1-6): ")
+
+        if choice == "1":
+            print("Your grocery list:")
+            if grocery_list:
+                for i, item in enumerate(grocery_list, 1):
+                    print(f"{i}. {item}")
+            else:
+                print("Your Grocery List is empty.")
+
+
+        if choice == "2":
+            item = input("Enter the item to add: ").strip()
+            if item:
+                grocery_list.append(item)
+                print(f"'{item}'added to the list.")
+            else:
+                print("Item cannot be empty.")
+
+        if choice == "3":
+            item = input("Enter an item to remove: ").strip
+            if item:
+                grocery_list.remove(item)
+                print(f"'{item}'removed from the list.")
