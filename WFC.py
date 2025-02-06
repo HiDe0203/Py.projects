0fd7adbf9a43ed7edd402cace3358caae607adab
Kunal Singh <kunal_singh@outlook.com>
2025-02-06 13:42:36 +0530

    Update WFC.py

--- WFC.py
+++ WFC.py
@@ -4,6 +4,15 @@ def wfc():
     import string
     for char in string.punctuation:
         paragraph = paragraph.replace(char, "")
-        print(paragraph)
+    words = paragraph.split()
+
+    word_count = {}
+
+    for word in words:
+        word_count[word] = word_count.get(word, 0) + 1
+
+    print("\nWord Frequency:")
+    for word, count in word_count.items():
+        print(f"{word}: {count}")
 
 wfc()
